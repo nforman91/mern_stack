4211fc00a64cb353c5dfdfe2d9bf794f5e9ea435
Nathan <nathan.forman@gmail.com>
2023-02-06 17:18:35 -0800

    Styling topbar and enabling FontAwesome icons

--- client/src/components/Topbar.jsx
+++ client/src/components/Topbar.jsx
@@ -1,8 +1,56 @@
 // import { Link } from "react-router-dom";
-// import styled from "styled-components";
+import styled from "styled-components";
 
 export default function Topbar() {
   return (
-    <div>Topbar</div>
+    <StyledTop>
+        <StyledTopLeft>
+            <StyledTopIcon className="fa-brands fa-square-facebook"></StyledTopIcon>
+            <StyledTopIcon className="fa-brands fa-square-twitter"></StyledTopIcon>
+            <StyledTopIcon className="fa-brands fa-square-instagram"></StyledTopIcon>
+        </StyledTopLeft>
+        <StyledTopCenter>
+            Topcenter
+        </StyledTopCenter>
+        <StyledTopRight>
+            Topright
+        </StyledTopRight>
+    </StyledTop>
   )
-}
+};
+
+const StyledTop = styled.div`
+    width: 100%;
+    height: 50px;
+    background-color: white;
+    position: sticky;
+    top: 0;
+    display: flex;
+    align-items: center;
+    z-index: 999;
+`;
+
+const StyledTopLeft = styled.div`
+    flex: 3;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+`;
+
+const StyledTopCenter = styled.div`
+    flex: 6;
+`;
+
+const StyledTopRight = styled.div`
+    flex: 3;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+`;
+
+const StyledTopIcon = styled.i`
+    font-size: 20px;
+    margin-right: 10px;
+    color: #444;
+    cursor: pointer;
+`;
