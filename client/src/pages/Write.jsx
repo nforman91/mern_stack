c7333992f6f36f98f9b6a66d6e538a75efef71b7
Nathan <nathan.forman@gmail.com>
2023-02-06 17:35:48 -0800

    Working on Write page

--- client/src/pages/Write.jsx
+++ client/src/pages/Write.jsx
@@ -2,10 +2,24 @@ import styled from "styled-components";
 
 export default function Write() {
   return (
-    <StyledWrite>Write</StyledWrite>
+    <StyledWrite>
+        <form className="writeForm">
+            <div className="writeFormGroup">
+                <label htmlFor="fileInput">
+                    <i className="writeIcon fa-solid fa-plus"></i>
+                </label>
+                <input type="file" id="fileInput" style={{display:"none"}}/>
+                <input type="text" placeholder="Title" className="writeInput" autoFocus={true}/>
+            </div>
+            <div className="writeFormGroup">
+                <textarea placeholder="Write your post here!" type="text" className="writeInput writeText"></textarea>
+            </div>
+            <button className="writeSubmit">Publish</button>
+        </form>
+    </StyledWrite>
   )
 }
 
 const StyledWrite = styled.div`
-    padding-top: 10px;
+    padding-top: 50px;
 `;
