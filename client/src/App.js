a3b85baa50a84055f0fd654237069b4513d32d66
Nathan <nathan.forman@gmail.com>
2023-02-06 17:05:00 -0800

    Changed app title in tab

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,11 @@
+import Topbar from "./components/Topbar";
 import Home from "./pages/Home";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <Router>
+      <Topbar/>
       <Routes>
         <Route exact path="/" element={<Home/>}/>
       </Routes>
