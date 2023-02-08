259d35bfde4d164414b80753717b4ba4a86c4d98
Nathan <nathan.forman@gmail.com>
2023-02-08 12:24:56 -0800

    Filled in Header component

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,7 +1,21 @@
-// import styled from "styled-components";
+import styled from "styled-components";
 
 export default function Header() {
   return (
-    <div>Header</div>
+    <StyledHeader>
+        <StyledHeaderTitle>Blog</StyledHeaderTitle>
+    </StyledHeader>
   )
 }
+
+const StyledHeader = styled.div`
+    margin-top: 60px;
+`;
+
+const StyledHeaderTitle = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    color: #444;
+    font-size: 100px;
+`;
