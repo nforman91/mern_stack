5795cd259b08aab8371acc464c01408cd275f28e
Nathan <nathan.forman@gmail.com>
2023-02-08 14:22:24 -0800

    Trying to connect routes and models

--- api/index.js
+++ api/index.js
@@ -2,8 +2,9 @@ const express = require("express");
 const app = express();
 const dotenv = require("dotenv");
 const mongoose = require("mongoose");
-const authRoute = require("");
-const postsRoute = require("");
+const authRoute = require("./routes/auth");
+const usersRoute = require("./routes/users");
+const postsRoute = require("./routes/posts");
 const multer = require("multer");
 
 dotenv.config();
@@ -29,4 +30,9 @@ app.post("/api/upload", upload.single("file"), (req, res) => {
 });
 
 app.use("/api/auth", authRoute);
+app.use("/api/users", usersRoute);
 app.use("/api/posts", postsRoute);
+
+app.listen("5000", () => {
+    console.log("Backend is running.")
+});
