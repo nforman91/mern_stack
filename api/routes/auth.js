3b9305c16c9047b489c57d52ad7d7b27f5c970e5
Nathan <nathan.forman@gmail.com>
2023-02-08 16:28:35 -0800

    Trying to access backend via Postman

--- api/routes/auth.js
+++ api/routes/auth.js
@@ -10,7 +10,7 @@ router.post("/register", async (req, res) => {
         const newUser = new User({
             username: req.body.username, 
             email: req.body.email, 
-            password: hashedPass,
+            password: hashedPass
         })
 
         const user = await newUser.save();
