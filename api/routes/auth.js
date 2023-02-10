38b1f923bac8d4872d56b9b051c8d7a2b2d28d64
Nathan <nathan.forman@gmail.com>
2023-02-10 13:54:50 -0800

    Established connection using Postman now adding Login route

--- api/routes/auth.js
+++ api/routes/auth.js
@@ -19,6 +19,22 @@ router.post("/register", async (req, res) => {
     catch(err){
         res.status(500).json(err);
     }
-})
+});
+
+// LOGIN
+router.post("/login", async (req, res) => {
+    try{
+        const user = await User.findOne({username: req.body.username})
+        !user && res.status(400).json("Wrong credentials!")
+
+        const validate = await bcrypt.compare(req.body.password, user.password);
+        !validate && res.status(400).json("Wrong credentials!")
+
+        const {password, ...others} = user._doc;
+        res.status(200).json(others);
+    }catch(err){
+        res.status(500).json(err)
+    }
+});
 
 module.exports = router;
