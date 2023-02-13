b38f2ec6f06b0a0d26f796496f936e5374fd5d72
Nathan <nathan.forman@gmail.com>
2023-02-13 15:41:07 -0800

    Added image to header

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -4,6 +4,7 @@ export default function Header() {
   return (
     <StyledHeader>
         <StyledHeaderTitle>Blog</StyledHeaderTitle>
+        <StyledHeaderImg src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b6/Image_created_with_a_mobile_phone.png/440px-Image_created_with_a_mobile_phone.png" alt="phone pic"/>
     </StyledHeader>
   )
 }
@@ -18,4 +19,11 @@ const StyledHeaderTitle = styled.div`
     align-items: center;
     color: #444;
     font-size: 100px;
+`;
+
+const StyledHeaderImg = styled.img`
+    width: 100%;
+    height: 450px;
+    margin-top: 80px;
+    object-fit: cover;
 `;
